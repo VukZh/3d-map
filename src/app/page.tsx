456b0d1cfb83919192f5f12088de9f14ca77bc27
VukZh <vukfx@inbox.ru>
2024-08-11 02:00:06 +0500

    chore: install nextjs, tailwindcss, eslint, prettier, typescript, shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+export default function Home() {
+  return (
+    <main>
+      <div className="flex items-center justify-center min-h-screen">
+        <Button>Button</Button>
+      </div>
+    </main>
+  );
+}
